420d252a96d543c157ffbd893db48ee551ac70a3
ADjenkov <ADjenkov>
2018-11-28 09:53:23 +0200

    Make --bundle friendly

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
     },
     {
         path: "tabs",
-        loadChildren: "./app/tabs/tabs.module#TabsModule"
+        loadChildren: "~/app/tabs/tabs.module#TabsModule"
     },
 ];
 
--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -15,13 +15,13 @@ import { TabsComponent } from "./tabs.component";
                         path: "players",
                         outlet: "playerTab",
                         component: NSEmptyOutletComponent,
-                        loadChildren: "./app/player/players.module#PlayersModule",
+                        loadChildren: "~/app/player/players.module#PlayersModule",
                     },
                     {
                         path: "teams",
                         outlet: "teamTab",
                         component: NSEmptyOutletComponent,
-                        loadChildren: "./app/team/teams.module#TeamsModule"
+                        loadChildren: "~/app/team/teams.module#TeamsModule"
                     }
                 ]
             }
